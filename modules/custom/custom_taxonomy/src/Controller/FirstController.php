a19dc635ded857824a24cc73e9cdc594700337e5
osseed <gawas.revati@gmail.com>
2017-10-26 19:46:18 +0530

    Added Custom taxonomy module

---
+++ modules/custom/custom_taxonomy/src/Controller/FirstController.php
@@ -0,0 +1,18 @@
+<?php
+// /**
+//  * @file
+//  * Contains \Drupal\custom_taxonomy\Controller\FirstController.
+//  */
+//
+// namespace Drupal\custom_taxonomy\Controller;
+//
+// use Drupal\Core\Controller\ControllerBase;
+//
+// class FirstController extends ControllerBase {
+//   public function content() {
+//     return array(
+//       '#type' => 'markup',
+//       '#markup' => t('Hello world'),
+//     );
+//   }
+// }
