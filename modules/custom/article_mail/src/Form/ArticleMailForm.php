44ef62fb15e89759bb9eda18a49f3d40f161590f
madhavimalgaonkar <madhavim2694@gmail.com>
2017-10-27 18:55:20 +0530

    Added replacement tokens in message subject and body

--- modules/custom/article_mail/src/Form/ArticleMailForm.php
+++ modules/custom/article_mail/src/Form/ArticleMailForm.php
@@ -24,21 +24,46 @@ class ArticleMailForm extends ConfigFormBase {
   public function getFormId() {
     return 'article_mail_form';
   }
+
+  function token_theme() {
+  $info['token_tree_link'] = [
+    'variables' => [
+    'token_types' => [],
+    'global_types' => FALSE,
+    'click_insert' => TRUE,
+    'show_restricted' => FALSE,
+    'show_nested' => FALSE,
+    'recursion_limit' => 3,
+    'text' => NULL,
+    'options' => [],
+  ],
+  'file' => 'token.pages.inc',
+  ];
+
+  return $info;
+}
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('article_mail.settings');
+     $config = $this->config('article_mail.settings');
     $form['subject'] = array(
       '#type' => 'textfield',
       '#title' => $this->t('Subject'),
-      '#default_value' => $config->get('Subject'),
+      '#default_value' => $config->get('subject'),
     );
     $form['body'] = array(
       '#type' => 'text_format',
       '#title' => $this->t('Body'),
-      '#default_value' => $config->get('Body'),
+      '#default_value' => $config->get('body'),
     );
+    // Add the token tree UI.
+    $form['body']['token_tree'] = array(
+  '#theme' => 'token_tree_link',
+  '#token_types' => array('user'),
+  '#show_restricted' => TRUE,
+  '#weight' => 90,
+);
     return parent::buildForm($form, $form_state);
   }
 
