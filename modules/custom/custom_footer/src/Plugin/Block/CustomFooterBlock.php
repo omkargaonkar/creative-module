b73f3222f14b574e9f63c9ee52cfdf2fb4687f1f
pranav45 <pranav.mainkar896@gmail.com>
2017-10-27 20:14:56 +0530

    Update custom module configuration[#150956943]

---
+++ modules/custom/custom_footer/src/Plugin/Block/CustomFooterBlock.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\custom_footer\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Hello' Block.
+ *
+ * @Block(
+ *   id = "CustomfooterBlock",
+ *   admin_label = @Translation("Custom Footer"),
+ *   category = @Translation("Custom Footer"),
+ * )
+ */
+class CustomFooterBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $config = \Drupal::config('custom_footer.settings');
+    return array(
+      '#markup' => $config->get('siteinfo_footer_setting'),
+    );
+  //  $storage = $form_state->getStorage();
+
+  }
+
+}
