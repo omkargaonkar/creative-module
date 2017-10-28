47d056c57d07c2f06c373827114dd317c03153b1
madhavimalgaonkar <madhavim2694@gmail.com>
2017-10-28 12:54:01 +0530

    Changed the menu link location and set default values

--- modules/custom/article_mail/src/Form/ArticleMailForm.php
+++ modules/custom/article_mail/src/Form/ArticleMailForm.php
@@ -13,69 +13,74 @@ class ArticleMailForm extends ConfigFormBase {
   /**
    * {@inheritdoc}
    */
-  protected function getEditableConfigNames() {
-    return [
-      'article_mail.settings',
-    ];
-  }
-  /**
-   * {@inheritdoc}
-   */
-  public function getFormId() {
-    return 'article_mail_form';
-  }
+   protected function getEditableConfigNames() {
+     return [
+       'article_mail.settings',
+     ];
+   }
 
-  function token_theme() {
-  $info['token_tree_link'] = [
-    'variables' => [
-    'token_types' => [],
-    'global_types' => FALSE,
-    'click_insert' => TRUE,
-    'show_restricted' => FALSE,
-    'show_nested' => FALSE,
-    'recursion_limit' => 3,
-    'text' => NULL,
-    'options' => [],
-  ],
-  'file' => 'token.pages.inc',
-  ];
+   /**
+    * {@inheritdoc}
+    */
+    public function getFormId() {
+      return 'article_mail_form';
+    }
 
-  return $info;
-}
-  /**
-   * {@inheritdoc}
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-     $config = $this->config('article_mail.settings');
-    $form['subject'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Subject'),
-      '#default_value' => $config->get('subject'),
-    );
-    $form['body'] = array(
-      '#type' => 'text_format',
-      '#title' => $this->t('Body'),
-      '#default_value' => $config->get('body'),
-    );
-    // Add the token tree UI.
-    $form['body']['token_tree'] = array(
-  '#theme' => 'token_tree_link',
-  '#token_types' => array('user'),
-  '#show_restricted' => TRUE,
-  '#weight' => 90,
-);
-    return parent::buildForm($form, $form_state);
-  }
+    /**
+     * {@inheritdoc}
+     */
+     function token_theme() {
+       $info['token_tree_link'] = [
+         'variables' => [
+           'token_types' => [],
+           'global_types' => TRUE,
+           'click_insert' => TRUE,
+           'show_restricted' => FALSE,
+           'show_nested' => FALSE,
+           'recursion_limit' => 3,
+           'text' => NULL,
+           'options' => [],
+         ],
+         'file' => 'token.pages.inc',
+       ];
+       return $info;
+     }
 
-  /**
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-    parent::submitForm($form, $form_state);
-    $this->config('article_mail.settings')
-      ->set('subject', $form_state->getValue('subject'))
-      ->set('body', $form_state->getValue('body'))
-      ->save();
+     /**
+      * {@inheritdoc}
+      */
+      public function buildForm(array $form, FormStateInterface $form_state) {
+        $config = $this->config('article_mail.settings');
+        $form['subject'] = array(
+          '#type' => 'textfield',
+          '#title' => $this->t('Subject'),
+          '#default_value' => $config->get('subject'),
+        );
+        $form['body'] = array(
+          '#type' => 'text_format',
+          '#title' => $this->t('Body'),
+          '#default_value' => $config->get('body'),
+        );
 
-  }
-}
+        // Add the token tree UI.
+        $form['body']['token_tree'] = array(
+          '#theme' => 'token_tree_link',
+          '#token_types' => array('user'),
+          '#show_restricted' => TRUE,
+          '#weight' => 90,
+        );
+        return parent::buildForm($form, $form_state);
+      }
+
+      /**
+       * {@inheritdoc}
+       */
+       public function submitForm(array &$form, FormStateInterface $form_state) {
+         parent::submitForm($form, $form_state);
+         $mail_body = $form_state->getValue('body');
+         $this->config('article_mail.settings')
+         ->set('subject', $form_state->getValue('subject'))
+         ->set('body', $mail_body['value'])
+         ->save();
+       }
+     }
